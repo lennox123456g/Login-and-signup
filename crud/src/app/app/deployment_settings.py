acbcf9d201112dd2d4410f1b779ccf8f0eaace8c
lennox123456g <lennomugumira12@gmail.com>
2025-07-29 10:35:28 +0300

    populating the cohrs header list

--- crud/src/app/app/deployment_settings.py
+++ crud/src/app/app/deployment_settings.py
@@ -73,7 +73,7 @@ AUTH_USER_MODEL = 'appsot.UserAccount'  # Remove the comma at the end
 #added
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
-    ''
+    'https://render-deploy-tutorial-reactjs-code.onrender.com'
 ]
 
 #Rest Framework
